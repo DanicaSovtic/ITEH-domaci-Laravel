337e97b90d13873c406468d6970eb7edacb9463b
DanicaSovtic <danica.sovtic@gmail.com>
2022-01-09 20:13:50 +0100

    Dodat model drzavljanstvo

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,10 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
-    protected $fillable = ['first_name', 'last_name', 'gender', 'nationality', 'city_id'];
-    protected $with = ['city'];
+    protected $fillable = ['first_name', 'last_name', 'gender', 'nationality', 'city_id', 'citizenship_id'];
+    protected $with = ['city', 'citizenship'];
     public function city()
     {
         return $this->belongsTo(City::class);
+    }
+    public function citizenship()
+    {
+        return $this->belongsTo(Citizenship::class);
     }
 }
